d44878c12a3747967ca1f8f9273630280f28f67c
jaofreire <139642804+jaofreire@users.noreply.github.com>
2024-05-10 23:43:36 -0300

    Delete Uber-exam/KeyRepository directory

--- Uber-exam/KeyRepository/Keys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KeyRepository
-{
-    public class Keys
-    {
-        public static string spTransToken = "10c0b386a89c5d47879d16c67b743484552c1b81661f7afeccfda7bd268ccebb";
-
-    }
-}
